1b80ff0b2aa0c06b99544c3ed87e49be93bb99cc
AaronCoto <cotoaaron0303@gmail.com>
2023-11-21 14:37:44 -0600

    configuracion router2

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import Sidebar from './components/sideBar'
+
+
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
-import Home from './pages/home'
-import Details from './pages/details'
+import Home from '../pages/home'
+import Details from '../pages/details'
 
 function MyRoutes() {
   
   return (
     <>
     <Router>
-    <Sidebar />
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/details' element={<Details/>}/>
