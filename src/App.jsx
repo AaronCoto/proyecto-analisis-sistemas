c12dcb274be4874cede08c908b6fc2d5a019b450
AaronCoto <cotoaaron0303@gmail.com>
2023-11-21 14:33:04 -0600

    configuracion router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { useState } from 'react'
 import Sidebar from './components/sideBar'
-import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
-import Home from './pages/home'
-import Details from './pages/details'
+import MyRoutes from './routers/routes'
 
 function App() {
   
   return (
     <>
-    <Router>
-    <Sidebar />
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/details' element={<Details/>}/>
-    </Routes>
-    </Router>
+    <MyRoutes/>
       
     </>
   )
